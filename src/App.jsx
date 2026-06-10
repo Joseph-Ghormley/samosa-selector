3804c5817b28de9b2b1ffc77cb7a0e5483fd845b
Joseph Ghormley <54871175+Joseph-Ghormley@users.noreply.github.com>
2026-06-10 18:22:34 -0400

    Start Samosa Selector lab

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>Samosa Selector</h1>
+        <h2>Count:</h2>
+        <img
+          className="samosa"
+          src="/samosa.png"
+          alt="samosa"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default App;
